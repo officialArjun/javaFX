ff0e88f38d06388dd226552c3d06dadf0beb98a4
arjun-agnisys <arjun@agnisys.com>
2022-03-08 16:00:47 +0530

    init_Beta2
    beta2 initialize

--- src/treeviewdemo/TreeViewDemo.java
+++ src/treeviewdemo/TreeViewDemo.java
@@ -29,77 +29,8 @@ public class TreeViewDemo extends Application  {
     
     @Override
     public void start(Stage primaryStage) {
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//            
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        
-//        Scene scene = new Scene(root, 300, 250);
-//        
-//        primaryStage.setTitle("Hello World!");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-          primaryStage.setTitle("Tree View Sample");        
         
-        TreeItem<String> rootItem = new TreeItem<> ("AGNISYS PROJECT");
-        TreeItem<String> rootItem1 = new TreeItem<> ("PROJECT1");
-        TreeItem<String> rootItem2 = new TreeItem<> ("PROJECT2");
-        TreeItem<String> rootItem3 = new TreeItem<> ("PROJECT3");
-        
-        rootItem.getChildren().addAll(rootItem1,rootItem2,rootItem3);
-        rootItem.setExpanded(true);
-        for (int i = 1; i < 6; i++) {
-            TreeItem<String> item = new TreeItem<> ("file" + i);            
-            rootItem1.getChildren().add(item);
-        }
-        
-        for (int i = 1; i < 6; i++) {
-            TreeItem<String> item = new TreeItem<> ("file" + i);            
-            rootItem2.getChildren().add(item);
-        }for (int i = 1; i < 6; i++) {
-            TreeItem<String> item = new TreeItem<> ("file" + i);            
-            rootItem3.getChildren().add(item);
-        }
-      
-        ObservableList<TreeItem<String>> child=rootItem.getChildren();
-        for(TreeItem<String> c: child){
-            c.setExpanded(true);
-        }
-        TreeView<String> tree = new TreeView<> (rootItem);        
-        TreeItem<String> item=tree.getSelectionModel().getSelectedItem();
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(tree);
-        
-        primaryStage.setScene(new Scene(root, 900, 750));
-        primaryStage.show();
-        
-//        ObservableList<TreeItem<String>> children = 
-//        tree.getSelectionModel().getSelectedItem().getChildren();
-//
-//        for (TreeItem<String> child : children) {
-//            System.out.println(child.getValue());
-//        }
-//        
- 
-    
     }
-//    public static void myfunc(TreeItem t){
-//        if(t==null){
-//            return;
-//        }
-//    }
-//    public boolean isDirectory(){
-//        return false;
-//    }
     public static void main(String[]args){
         launch(args);
        
